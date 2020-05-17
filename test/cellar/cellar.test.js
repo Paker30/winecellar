296f1014513e5c9ee77b84c290d125277ec6eac9
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-05-17 19:11:42 +0200

    test(svg): handle static files
    update cellar test to work with svg

--- test/cellar/cellar.test.js
+++ test/cellar/cellar.test.js
@@ -31,21 +31,20 @@ configure({ adapter: new Adapter() });
 
 describe('All the bottles', () => {
     test('The cellar is empty', () => {
-        const cellar = shallow(<Cellar columns={columns} bottles={[]} adjustMainAreaWide={() => 2} />);
+        const cellar = shallow(<Cellar columns={columns} bottles={[]} />);
         expect(cellar.find('List').prop('dataSource')).toHaveLength(0);
     });
     test('There are bottles', () => {
         const bottleExample = {
-            color: 'red',
+            color: 'Red',
             id: 'bottle-ka2pdxeb',
             name: 'prado marina',
             type: 'wine',
             year: '2018',
             _doc_id_rev: 'ka2pdxec::1-55d50aa0131852bb2113838c6b55d269'
         };
-        const MockAdjustArea = jest.fn();
         const MockDeleteBottle = jest.fn();
-        const cellar = shallow(<Cellar columns={columns} bottles={[{bottle: bottleExample, title: 'prado marina'}]} adjustMainAreaWide={new MockAdjustArea()} deleteBootle={new MockDeleteBottle()} />);
+        const cellar = shallow(<Cellar columns={columns} bottles={[{ bottle: bottleExample, title: 'prado marina' }]} deleteBootle={new MockDeleteBottle()} />);
         expect(cellar.find('List').prop('dataSource')).toHaveLength(1);
         const bottle = cellar.find('List').render().find('.ant-list-item-meta-content');
         expect(bottle).toHaveLength(1);
