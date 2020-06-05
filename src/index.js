92841f409345bfd72c4029edb04b2d339505b695
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-06-05 19:37:41 +0200

    fix(i18n): init method returns a promise
    before start, the app has to start all is ready

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './i18n';
+import i18n from './i18n';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app'), //  eslint-disable-line no-undef
-);
+i18n
+    .init({
+        lng: 'es',
+        fallbackLng: 'en',
+        debug: process.env.NODE_ENV === 'development',
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        initImmediate: false
+    })
+    .then(() => {
+        ReactDOM.render(
+            <App />,
+            document.getElementById('app'), //  eslint-disable-line no-undef
+        );
 
-module.hot.accept();
+        module.hot.accept();
+    });
