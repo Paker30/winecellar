d49a56e501c4cd4d0b969bda8fa48c851e3ef0ef
Francisco <fmoreno@aligntech.com>
2023-01-02 19:14:34 +0100

    refactor(app): from class to function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import { Layout } from 'antd';
 import Uniqid from 'uniqid';
@@ -119,73 +119,44 @@ function PrivateRoute({ userLogged, children, ...rest }) {
     );
 }
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bottles: [],
-            searchByName: '',
-            user: { name: '' },
-            db: new PouchDB('cellar_db')
-        };
-        this.addBottle = this.addBottle.bind(this);
-        this.deleteBootle = this.deleteBootle.bind(this);
-        this.updateBottle = this.updateBottle.bind(this);
-        this.saveUser = this.saveUser.bind(this);
-        this.search = this.search.bind(this);
-    }
+const App = () => {
+    const [db] = useState(new PouchDB('cellar_db'));
+    const [bottles, setBottles] = useState([]);
+    const [searchByName, setSearchByName] = useState('');
+    const [user, setUser] = useState({ name: '' });
 
-    componentDidMount() {
-        const { db } = this.state;
-        retrieveAll(db)
-            .then(({ bottles, user }) => {
-                this.setState({ bottles });
-                this.setState({ user });
-            })
-            .catch((error) => {
-                console.error('Something went wrong fetching the bottles', error);
-                this.setState({ bottles: [] });
-            });
-    }
-
-    addBottle(bottle) {
-        const { bottles, db } = this.state;
+    const addBottle = (bottle) => {
         const bottleId = Uniqid('bottle-');
         const id = Uniqid();
         db.put({ ...bottle, _id: id, id: bottleId })
             .then(({ rev }) => {
-                this.setState({ bottles: [...bottles, { id: bottleId, _id: id, ...bottle, _rev: rev }] });
+                setBottles([...bottles, { id: bottleId, _id: id, ...bottle, _rev: rev }]);
             })
             .catch((error) => console.error('Something went wrong adding the bottle', error));
-    }
+    };
 
-    updateBottle(bottle) {
-        const { bottles, db } = this.state;
+    const updateBottle = (bottle) => {
         db.put(bottle)
             .then(({ rev }) => {
-                this.setState({ bottles: [...bottles.filter(({ id }) => id !== bottle.id), { ...bottle, _rev: rev }] });
+                setBottles([...bottles.filter(({ id }) => id !== bottle.id), { ...bottle, _rev: rev }]);
             })
             .catch((error) => console.error('Something went wrong updating the bottle', error));
-    }
+    };
 
-    deleteBootle(bottle) {
-        const { bottles, db } = this.state;
+    const deleteBootle = (bottle) => {
         const bottleRemove = bottles.find(({ id }) => id === bottle.id);
         db.remove(bottleRemove)
-            .then(() => this.setState({ bottles: [...bottles.filter(({ id }) => id !== bottle.id)] }))
+            .then(() => setBottles([...bottles.filter(({ id }) => id !== bottle.id)]))
             .catch((error) => console.error('Something went wrong removing the bottle', error));
-    }
+    };
 
-    saveUser(user) {
-        const { db } = this.state;
+    const saveUser = () => {
         db.put({ _id: Uniqid('user-'), id: Uniqid(), name: user })
-            .then(() => this.setState({ user: { name: user } }))
+            .then(() => setUser({ name: user }))
             .catch((error) => console.error('Something went wrong saving your name', error));
-    }
+    };
 
-    search(value) {
-        const { bottles } = this.state;
-
+    const search = (value) => {
         const filteredBottles = bottles.filter(({ name }) => sameName(name)(value));
 
         if (filteredBottles.length) {
@@ -193,79 +164,91 @@ export default class App extends Component {
         }
 
         return bottles;
-    }
+    };
 
-    render() {
-        const { bottles, user, searchByName } = this.state;
-
-        const mainContent = user.name
-            ? (
-                <Cellar
-                    bottles={this.search(searchByName).map((bottle) => ({ bottle, title: <Link to={`/bottle?id=${bottle.id}`}>{bottle.name}</Link> }))}
-                />
-            )
-            : (
-                <Login saveUser={this.saveUser} />
-            );
-        return (
-            <Area>
-                <Router>
-                    <HeaderArea>
-                        <Title userName={user} />
-                        <ToolBar
-                            home={homeLink}
-                            add={addLink}
-                            search={<Search onSearch={(value) => this.setState({ searchByName: value })} />}
-                        />
-                    </HeaderArea>
-                    <MainArea>
-                        <ListArea>
-                            <Switch>
-                                <Route path="/">
-                                    <Suspense fallback={<div><Trans i18nKey="loading" /></div>}>
-                                        {mainContent}
-                                    </Suspense>
-                                </Route>
-                            </Switch>
-                        </ListArea>
-                        <DetailArea>
-                            <Switch>
-                                <Route path="/bottle">
-                                    <Suspense fallback={<Loading />}>
-                                        <Bottle find={pickBottle(bottles)} deleteBootle={this.deleteBootle} />
-                                    </Suspense>
-                                </Route>
-                                <PrivateRoute userLogged={user.name} path="/add">
-                                    <Suspense fallback={<Loading />}>
-                                        <OtherBottle add={this.addBottle} find={pickBottle(bottles)} />
-                                    </Suspense>
-                                </PrivateRoute>
-                                <Route path="/edit">
-                                    <Suspense fallback={<Loading />}>
-                                        <OtherBottle add={this.updateBottle} find={pickBottle(bottles)} />
-                                    </Suspense>
-                                </Route>
-                            </Switch>
-                        </DetailArea>
-                    </MainArea>
-                    <FooterArea>
-                        <Footer style={{ display: 'flex' }}>
-                            <span>
-                                Icons and design by &nbsp;
-                                <a
-                                    href="https://www.instagram.com/melapbq/?hl=es"
-                                    style={{ color: 'inherit' }}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                >
-                                    Mel
-                                </a>
-                            </span>
-                            <span style={{ marginLeft: 'auto' }}>{`v ${version}`}</span>
-                        </Footer>
-                    </FooterArea>
-                </Router>
-            </Area>
+    const mainContent = user.name
+        ? (
+            <Cellar
+                bottles={search(searchByName).map((bottle) => ({ bottle, title: <Link to={`/bottle?id=${bottle.id}`}>{bottle.name}</Link> }))}
+            />
+        )
+        : (
+            <Login saveUser={saveUser} />
         );
-    }
-}
+
+    useEffect(() => {
+        retrieveAll(db)
+            // eslint-disable-next-line no-shadow
+            .then(({ bottles, user }) => {
+                setBottles(bottles);
+                setUser(user);
+            })
+            .catch((error) => {
+                console.error('Something went wrong fetching the bottles', error);
+                setBottles([]);
+            });
+    }, []);
+
+    return (
+        <Area>
+            <Router>
+                <HeaderArea>
+                    <Title userName={user} />
+                    <ToolBar
+                        home={homeLink}
+                        add={addLink}
+                        search={<Search onSearch={setSearchByName} />}
+                    />
+                </HeaderArea>
+                <MainArea>
+                    <ListArea>
+                        <Switch>
+                            <Route path="/">
+                                <Suspense fallback={<div><Trans i18nKey="loading" /></div>}>
+                                    {mainContent}
+                                </Suspense>
+                            </Route>
+                        </Switch>
+                    </ListArea>
+                    <DetailArea>
+                        <Switch>
+                            <Route path="/bottle">
+                                <Suspense fallback={<Loading />}>
+                                    <Bottle find={pickBottle(bottles)} deleteBootle={deleteBootle} />
+                                </Suspense>
+                            </Route>
+                            <PrivateRoute userLogged={user.name} path="/add">
+                                <Suspense fallback={<Loading />}>
+                                    <OtherBottle add={addBottle} find={pickBottle(bottles)} />
+                                </Suspense>
+                            </PrivateRoute>
+                            <Route path="/edit">
+                                <Suspense fallback={<Loading />}>
+                                    <OtherBottle add={updateBottle} find={pickBottle(bottles)} />
+                                </Suspense>
+                            </Route>
+                        </Switch>
+                    </DetailArea>
+                </MainArea>
+                <FooterArea>
+                    <Footer style={{ display: 'flex' }}>
+                        <span>
+                            Icons and design by &nbsp;
+                            <a
+                                href="https://www.instagram.com/melapbq/?hl=es"
+                                style={{ color: 'inherit' }}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                Mel
+                            </a>
+                        </span>
+                        <span style={{ marginLeft: 'auto' }}>{`v ${version}`}</span>
+                    </Footer>
+                </FooterArea>
+            </Router>
+        </Area>
+    );
+};
+
+export default App;
